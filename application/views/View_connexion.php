fbab0491087c3c0c28259e55b2a66f36d8ad5494
raymonde <enveric.raymond@etu.u-pec.fr>
2021-05-02 21:04:55 +0200

    Ajout d'une modale d'information et modification du contact par mail deView_connexion.php

--- application/views/View_connexion.php
+++ application/views/View_connexion.php
@@ -21,18 +21,41 @@ $this->load->helper('form');
    <div class="collapse navbar-collapse">
         <ul class="navbar-nav ml-auto">
           <li class="nav-item active">
-            <a class="nav-link text-white hov" target="_top" href="mailto:Le.covidetecteur@gmail.com?subject=">Contact
-                  <span class="sr-only">(current)</span>
-                </a>
+            <a class="nav-link text-white hov" target="_top" href = "mailto: le.covidetecteur@gmail.com">Contact
+           		<span class="sr-only">(current)</span>
+       		</a>
           </li>
           <li class="nav-item">
-            <a class="hov" href="<?php echo base_url()?>">
+   			<!-- Boutton qui actionne le modale (pop-up)-->
+			<a data-toggle="modal" data-target="#exampleModalCenter">
               <img src="<?php echo base_url()?>/images/icon-info.png" alt="logo" style="width:75%" >
             </a>
           </li>
         </ul>
       </div>
 </nav>
+
+<!-- Modale (pop-up) -->
+<div class="modal fade" id="exampleModalCenter" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
+  <div class="modal-dialog modal-dialog-centered" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h3 class="modal-title" id="exampleModalLongTitle">Informations sur Le Covidétecteur</h3>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <div class="modal-body">
+        <p><b>Ce site va vous permettre de connecter votre capteur Covidétecteur afin de visualiser ses données.</b></p>
+        <p>Le Covidétecteur est un appareil capable de détecter et d'alerter sur la présence potentielle de charge virale dans l’air d’un lieu clos.</p>
+        <p>Le Covidétecteur est équipé d'un capteur Sensirion SCD30 utilisant la technologie CMOSens® pour la détection infrarouge qui permet la mesure extrêmement précise de dioxyde de carbone. Outre la technologie de mesure NDIR pour la détection du CO2, un capteur d'humidité et de température Sensirion de qualité supérieure est également intégré sur le même module capteur.</p>
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-primary" data-dismiss="modal">Fermer</button>
+      </div>
+    </div>
+  </div>
+</div>
 
 <!-- Page Content -->
   <div class="container" style="width:50%">
