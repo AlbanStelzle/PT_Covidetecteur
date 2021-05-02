fea60952a913be3bfde260bba68b58da348552bc
raymonde <enveric.raymond@etu.u-pec.fr>
2021-05-02 18:49:22 +0200

    mise à jour View_confirmation.php

--- application/views/View_confirmation.php
+++ application/views/View_confirmation.php
@@ -1,3 +1,7 @@
+<head>
+<title>Le Covidétecteur</title>
+</head>
+
 <?php
 $this->load->helper('form');
 ?>
@@ -15,21 +19,21 @@ $this->load->helper('form');
 <nav class="navbar navbar-expand-sm">
     <ul class="navbar-nav">
         <li class="nav-item active">
-            <a>
-                <img src="<?php echo base_url()?>images/icon-home.png" alt="logo" style="width:50%">
+            <a class="hov" href="<?php echo base_url()?>">
+                <img class="hov" src="<?php echo base_url()?>images/icon-home.png" alt="logo" style="width:50%">
             </a>
         </li>
     </ul>
     <div class="collapse navbar-collapse" id="navbarResponsive">
         <ul class="navbar-nav ml-auto">
             <li class="nav-item active">
-                <a class="nav-link text-white" target="_top" href="mailto:your@email.address?subject=">Contact
+                <a class="nav-link text-white hov" target="_top" href="mailto:your@email.address?subject=">Contact
                     <span class="sr-only">(current)</span>
                 </a>
             </li>
             <li class="nav-item">
                 <a href="">
-                    <img src="<?php echo base_url()?>images/icon-info.png" alt="logo" style="width:75%" >
+                    <img class="hov" src="<?php echo base_url()?>images/icon-info.png" alt="logo" style="width:75%" >
                 </a>
             </li>
         </ul>
@@ -38,28 +42,26 @@ $this->load->helper('form');
 
 <!-- LOGO -->
 <div class="text-center">
-    <h1>Votre inscription a bien été prise en compte</h1>
+    <h1 class="confirmation-text">Votre inscription a bien été prise en compte</h1>
     <br>
     <br>
-    <img src="<?php echo base_url()?>images/icon-check-2.png" alt="logo">
+    <img class="logo-confirmation" src="<?php echo base_url()?>images/icon-check-2.png" alt="logo">
 </div>
 
 <br>
 <br>
 
-<?php
-$seconnecter = array(
-    'class'=>'btn btn-success rounded-pill',
-    'value' => 'true',
-    'content' => 'Se connecter'
-);
+<div class="button-center" onClick="location.href='http://ptcovidetecteur/index.php/Accueil/register'">
+    <?php
+    $seconnecter = array(
+        'class'=>'btn btn-success rounded-pill col-sm-2',
+        'value' => 'true',
+        'content' => 'Se connecter'
+    );
 
-
-
-echo form_button($seconnecter);
-?>
-
-
+    echo form_button($seconnecter);
+    ?>
+</div>
 
 <footer>
 </footer>
