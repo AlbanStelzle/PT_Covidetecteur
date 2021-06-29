743e0ea3c5cb3265e7fd1978651ab9e25bcc8a7c
raymonde <enveric.raymond@etu.u-pec.fr>
2021-06-29 22:48:19 +0200

    modifications pour affichage smartphone

--- application/views/View_menu.php
+++ application/views/View_menu.php
@@ -2,42 +2,65 @@
 <html>
 <body>
 
+<body>
+
   <!-- Navigation -->
   <nav class="navbar navbar-expand-sm">
     <ul class="navbar-nav">
       <li class="nav-item active">
-        <a class="hoov" href="">
-         <img src="<?php echo base_url()?>images/icon-home.png" style="width:50%">
-       </a>
-       <li>
-        <h2 class="text-white" style="margin-top: 10px;">MES APPAREILS ></h2>
+        <a class="hov" href="<?php echo base_url()?>">
+          <img src="<?php echo base_url()?>/images/icon-home.png" class="icon-home-capteur">
+        </a>
       </li>
-    </li>
-
+      <li>
+        <h2 class="text-white" style="margin-top: 10px;">MES APPAREILS &gt;</h2>
+      </li>
     </ul>
     <div class="collapse navbar-collapse">
       <ul class="navbar-nav ml-auto">
         <li class="nav-item active">
-          <a class="nav-link text-white hoov" target="_top" href="mailto:Le.covidetecteur@gmail.com?subject=">Contact
-            <span class="sr-only">(current)</span>
+          <a class="nav-link text-white hov" target="_top" href = "mailto: le.covidetecteur@gmail.com">Contact
+           <span class="sr-only">(current)</span>
           </a>
         </li>
         <li class="nav-item">
-          <a class="hoov" href="">
-            <img src="<?php echo base_url()?>/images/icon-info.png" alt="logo" style="width:75%" >
-          </a>
+          <!-- Boutton qui actionne le modale (pop-up)-->
+          <a data-toggle="modal" data-target="#exampleModalCenter">
+            <img class="hov" src="<?php echo base_url()?>images/icon-info.png" alt="logo" style="width:75%" >
+           </a>
         </li>
       </ul>
     </div>
   </nav>
-  <!-- Page Content -->
-  <br>
-  <br>
-  <br>
 
+
+  <!-- Modale (pop-up) -->
+  <div class="modal fade" id="exampleModalCenter" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
+    <div class="modal-dialog modal-dialog-centered" role="document">
+      <div class="modal-content">
+        <div class="modal-header">
+          <h3 class="modal-title" id="exampleModalLongTitle">Informations sur Le Covidétecteur</h3>
+          <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+            <span aria-hidden="true">&times;</span>
+          </button>
+        </div>
+        <div class="modal-body">
+          <p><b>Ce site va vous permettre de connecter votre capteur Covidétecteur afin de visualiser ses données.</b></p>
+          <p>Le Covidétecteur est un appareil capable de détecter et d'alerter sur la présence potentielle de charge virale dans l’air d’un lieu clos.</p>
+          <p>Le Covidétecteur est équipé d'un capteur Sensirion SCD30 utilisant la technologie CMOSens® pour la détection infrarouge qui permet la mesure extrêmement précise de dioxyde de carbone. Outre la technologie de mesure NDIR pour la détection du CO2, un capteur d'humidité et de température Sensirion de qualité supérieure est également intégré sur le même module capteur.</p>
+        </div>
+        <div class="modal-footer">
+          <button type="button" class="btn btn-primary" data-dismiss="modal">Fermer</button>
+        </div>
+      </div>
+    </div>
+  </div>
+
+
+  <!-- Page Content -->
 
   <!-- 1er container contenant 3 cartes blanche -->
-  <div class="container" style="width:90%">
+  <div class="container taille-carte">
 
 
         <!-- première carte blanche du premier container -->
@@ -50,7 +73,7 @@
 		  	if( $i == 0) {
 				echo "<div class=\"card-group\">";
 			}
-		  		echo '<div class="card border-0 shadow my-2 mx-2" style="height: 250px;">
+		  		echo '<div class="card border-0 shadow my-2 mx-2">
 				  <div class="card-body p-3" id="">
 
 					  <div class="text-center" >
@@ -88,12 +111,12 @@
 		  }
 		  }
 		  ?>
-          <div class="card border-0 shadow my-2 mx-2" style="height: 250px;">
+          <div class="card border-0 shadow my-2 mx-2 taille-carte-2">
             <div class="card-body p-3" id="white_card_1_1">
 
             <div class="text-center" >
               <a class="hoov" href="./add_detector">
-                <img src="<?php echo base_url()?>/images/icon-cross.png" alt="logo" style="width:25%" >
+                <img src="<?php echo base_url()?>/images/icon-cross.png" class="icon-croix" alt="logo">
               </a>
             </div>
 			</div>
