c50774c39f4db9a94e686ead7f6ea5f88e7f757d
raymonde <enveric.raymond@etu.u-pec.fr>
2021-06-30 01:20:40 +0200

    derniers ajustements et indentations

--- application/views/View_menu.php
+++ application/views/View_menu.php
@@ -13,7 +13,7 @@
         </a>
       </li>
       <li>
-        <h2 class="text-white" style="margin-top: 10px;">MES APPAREILS &gt;</h2>
+        <h2 class="titre-menu-capteurs">MES CAPTEURS &gt;</h2>
       </li>
     </ul>
     <div class="collapse navbar-collapse">
@@ -26,7 +26,7 @@
         <li class="nav-item">
           <!-- Boutton qui actionne le modale (pop-up)-->
           <a data-toggle="modal" data-target="#exampleModalCenter">
-            <img class="hov" src="<?php echo base_url()?>images/icon-info.png" alt="logo" style="width:75%" >
+            <img src="<?php echo base_url()?>images/icon-info.png" class="icon-info hov" alt="logo">
            </a>
         </li>
       </ul>
@@ -62,96 +62,66 @@
   <!-- 1er container contenant 3 cartes blanche -->
   <div class="container taille-carte">
 
+    <!-- première carte blanche du premier container -->
+	  <?php
+		//print_r($detecteur);
+	  $i = 0;
+	  if(isset($detecteur)){
+  	  foreach($detecteur as $key){
 
-        <!-- première carte blanche du premier container -->
-		  <?php
-			//print_r($detecteur);
-		  $i = 0;
-		  if(isset($detecteur)){
-		  foreach($detecteur as $key){
+  	  	if( $i == 0) {
+  			  echo "<div class=\"card-group\">";
+  		  }
+  	  	echo '<div class="card border-0 shadow my-2 mx-2">
+        <div class="card-body p-3" id="">
 
-		  	if( $i == 0) {
-				echo "<div class=\"card-group\">";
-			}
-		  		echo '<div class="card border-0 shadow my-2 mx-2">
-				  <div class="card-body p-3" id="">
+  			<div class="text-center" >
+  			<h5>'.$key["name"].' </h5>';
+  		  if($key["lastdata"] == null){
+  				echo '<div class="alert alert-primary" role="alert">Aucune donnée n\'est présente.</div>';
+  			}else if($key["lastdata"]>800 && $key["lastdata"] < 1500){
+  			echo '<div class="alert alert-warning" role="alert">
+        La qualité de l\'air est moyenne. La concentration de CO2 est de '.$key["lastdata"].' PPM. Il est préférable d\'aérer la pièce.
+        </div>';
+  			}else if($key["lastdata"] > 1500){
+  		  	echo '<div class="alert alert-danger" role="alert">
+          La qualité de l\'air est très mauvaise. La concentration de CO2 est de '.$key["lastdata"].' PPM. Il est urgent d\'aérer la pièce.
+          </div>';
+  			}else if($key["lastdata"] <800){
+  				echo '<div class="alert alert-success" role="alert">
+          La qualité de l\'air est bonne. La concentration de CO2 est de '.$key["lastdata"].' PPM.
+          </div>';
+  			}
+  			echo '<button class="btn btn-primary btn-sm mr-1">';
+        echo anchor('MenuPrincipal/Graph/'.$key["id"],'Voir les statistiques globales','class="btn btn-primary btn-sm"');
+        echo '</button>';
+  			echo '<button class="btn btn-danger mb-2 ml-1" style="margin-top:2%" onclick="location.href =\'MenuPrincipal/delete/'.$key["id"].'\'">Supprimer ce capteur</button>
+  			</div>
 
-					  <div class="text-center" >
-						  <h5>'.$key["name"].' </h5>
-						  ';
-		  	  	if($key["lastdata"] == null){
-					echo '<div class="alert alert-primary" role="alert">Aucune donnée n\'est présente.</div>';
-				}else if($key["lastdata"]>800 && $key["lastdata"] < 1500){
-					echo '<div class="alert alert-warning" role="alert">
-  La qualité de l\'air est moyenne. La concentration de CO2 est de '.$key["lastdata"].' PPM. Il est préférable d\'aérer la pièce.
-</div>';
-				}else if($key["lastdata"] > 1500){
-		  		echo '<div class="alert alert-danger" role="alert">
-  La qualité de l\'air est très mauvaise. La concentration de CO2 est de '.$key["lastdata"].' PPM. Il est urgent d\'aérer la pièce.
-</div>';
-			}else if($key["lastdata"] <800){
-				echo '<div class="alert alert-success" role="alert">
-  La qualité de l\'air est bonne. La concentration de CO2 est de '.$key["lastdata"].' PPM.
-</div>';
-			}
-						echo '
-						  <button class="btn btn-primary btn-sm mr-1">';
-              echo anchor('MenuPrincipal/Graph/'.$key["id"],'Voir les statistiques globales','class="btn btn-primary btn-sm"');
-              echo '</button>';
-						  echo '<button class="btn btn-danger mb-2 ml-1" style="margin-top:2%" onclick="location.href =\'MenuPrincipal/delete/'.$key["id"].'\'">Supprimer ce capteur</button>
-					  </div>
-
-				  </div>
-			  </div>';
-		  	if($i ==2){
-		  		echo '</div>';
-		  		$i=-1;
-			}
-		  	$i++;
-		  }
-		  }
-		  ?>
-          <div class="card border-0 shadow my-2 mx-2 taille-carte-2">
-            <div class="card-body p-3" id="white_card_1_1">
-
-            <div class="text-center" >
-              <a class="hoov" href="./add_detector">
-                <img src="<?php echo base_url()?>/images/icon-cross.png" class="icon-croix" alt="logo">
-              </a>
-            </div>
-			</div>
-
-          </div>
-
-    </div>
-
-            <br>
-            <br>
-            <br>
-
-
-
-          </div>
+  			</div>
+  			</div>';
+  		  if($i ==2){
+  		    echo '</div>';
+  		  	$i=-1;
+  			}
+  		  $i++;
+  		}
+	  }
+	  ?>
+    <div class="card border-0 shadow my-2 mx-2 taille-carte-2">
+      <div class="card-body p-3" id="white_card_1_1">
+        <div class="text-center" >
+          <a class="hoov" href="./add_detector">
+            <img src="<?php echo base_url()?>/images/icon-cross.png" class="icon-croix hov" alt="logo">
+          </a>
         </div>
-      </div>
-
-
-
-
+		  </div>
+    </div>
   </div>
 
-</div>
-
-
-
-
-
-
-
-
-
-
-</div>
+  <br>
+  <br>
+  <br>
 
 <!-- script -->
 <script src="<?php echo base_url()?>/js/cards.js"></script>
